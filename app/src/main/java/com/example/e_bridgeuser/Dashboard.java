91cfb657af616d4b9dcd1d2b558b7967c89605a0
Ashita vijay seth <ashita.seth_18@gla.ac.in>
2020-11-22 22:36:27 +0530

    bottom navigation bar designed

---
+++ app/src/main/java/com/example/e_bridgeuser/Dashboard.java
@@ -0,0 +1,24 @@
+package com.example.e_bridgeuser;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class Dashboard extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    NavController navController;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
+        navController = Navigation.findNavController(this, R.id.navigation_bar);
+    }
+}
