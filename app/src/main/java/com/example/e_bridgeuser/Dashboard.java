7b107dcc0e5b684448e7fab57987546a9238096b
Ashita vijay seth <ashita.seth_18@gla.ac.in>
2020-11-23 01:01:09 +0530

    drawer layout completed

--- app/src/main/java/com/example/e_bridgeuser/Dashboard.java
+++ app/src/main/java/com/example/e_bridgeuser/Dashboard.java
@@ -1,19 +1,33 @@
 package com.example.e_bridgeuser;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
-public class Dashboard extends AppCompatActivity {
+import java.util.Objects;
+
+public class Dashboard extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     BottomNavigationView bottomNavigationView;
     NavController navController;
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle toggle;
+    NavigationView navigationView;
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,7 +35,45 @@ public class Dashboard extends AppCompatActivity {
 
         bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
         navController = Navigation.findNavController(this, R.id.frame_layout);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        navigationView = (NavigationView) findViewById(R.id.navigationView);
+
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.start, R.string.end);
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        //can provoke null pointer exception
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        navigationView.setNavigationItemSelectedListener(this);
+
 
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.website) {
+            Toast.makeText(this, "Website", Toast.LENGTH_SHORT).show();
+
+        } else if (item.getItemId() == R.id.developers) {
+            Toast.makeText(this, "Developer", Toast.LENGTH_SHORT).show();
+
+
+        } else {
+            Toast.makeText(this, "Ebook", Toast.LENGTH_SHORT).show();
+
+        }
+
+
+        return true;
     }
 }
--- app/src/main/java/com/example/e_bridgeuser/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/e_bridgeuser/ui/home/HomeFragment.java
@@ -8,14 +8,65 @@ import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
 
 import com.example.e_bridgeuser.R;
+import com.smarteist.autoimageslider.DefaultSliderView;
+import com.smarteist.autoimageslider.IndicatorAnimations;
+import com.smarteist.autoimageslider.SliderAnimations;
+import com.smarteist.autoimageslider.SliderLayout;
 
 public class HomeFragment extends Fragment {
 
+    SliderLayout slider;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        slider = view.findViewById(R.id.slider);
+        slider.setIndicatorAnimation(IndicatorAnimations.COLOR);
+        slider.setSliderTransformAnimation(SliderAnimations.CUBEINDEPTHTRANSFORMATION);
+        slider.setScrollTimeInSec(2);
+
+
+        setSliderImages();
+
+
+        return view;
+    }
+
+    private void setSliderImages() {
+        //using for loops for multiple images
+        for (int i = 0; i < 5; i++) {
+            DefaultSliderView defaultSliderView = new DefaultSliderView(getContext());
+            switch (i) {
+                case 0:
+                    //using URL to so that our application is of small size
+                    defaultSliderView.setImageUrl("https://www.admissionfever.com/Media/clgimg/gallery/343_img7278888380707487.png");
+                    break;
+
+                case 1:
+                    defaultSliderView.setImageUrl("https://www.gla.ac.in/images/about-us.jpg");
+                    break;
+
+                case 2:
+                    defaultSliderView.setImageUrl("https://akm-img-a-in.tosshub.com/sites/indiacontent/0/images/product/public/27112019/00/01/57/48/54/66/39/13/1574854663913/659-gla-university-iin-image-GLA_University__Mathura__16_.jpg");
+                    break;
+
+                case 3:
+                    defaultSliderView.setImageUrl("https://getmyuni.azureedge.net/college-image/big/gla-university-mathura.jpg");
+                    break;
+
+                case 4:
+                    defaultSliderView.setImageUrl("https://www.indcareer.com/files/imagecache/900x/files/54561/img3281a.jpg");
+                    break;
+
+
+            }
+            //settingimage scale
+
+        }
+
+
     }
 }
