b041df6c5a460b838269f587296c89fecb405cbc
Ashita vijay seth <ashita.seth_18@gla.ac.in>
2020-11-23 23:11:26 +0530

    Recycler View for ebook module designed + ebook dat ajava class completed

---
+++ app/src/main/java/com/example/e_bridgeuser/ebook/EbookActivity.java
@@ -0,0 +1,16 @@
+package com.example.e_bridgeuser.ebook;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.e_bridgeuser.R;
+
+public class EbookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ebook);
+    }
+}
